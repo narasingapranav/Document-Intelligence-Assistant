dabd67eff72f66a1fba987866502dca4383ac1c4
narasingapranav <pranavch74934@gmail.com>
2026-06-14 16:42:25 +0530

    overcame problems

--- auth.py
+++ auth.py
@@ -2,9 +2,21 @@ import firebase_admin
 from firebase_admin import credentials, auth
 import streamlit as st
 
-cred = credentials.Certificate("serviceAccountKey.json")
-
 if not firebase_admin._apps:
+
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": st.secrets["FIREBASE_PROJECT_ID"],
+        "private_key_id": st.secrets["FIREBASE_PRIVATE_KEY_ID"],
+        "private_key": st.secrets["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+        "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
+        "client_id": "",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": ""
+    })
+
     firebase_admin.initialize_app(cred)
 
 
